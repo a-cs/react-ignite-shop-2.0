75d3eb7a4507b80324dcc489e9aeff800c4d1fce
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2023-07-27 14:15:47 -0300

    creating shopping cart icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,24 @@ import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
 import logoImg from "../assets/Logo.svg"
-import { Container, Header } from '../styles/pages/app'
+import { Container, Header, ShoppingCartIcon } from '../styles/pages/app'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Handbag } from '@phosphor-icons/react'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  	return (
+	return (
 		<Container>
 			<Header>
 				<Link href="/" >
 					<Image src={logoImg} alt="" />
 				</Link>
+				<ShoppingCartIcon>
+					<Handbag size={24} />
+					<span>1</span>
+				</ShoppingCartIcon>
 			</Header>
 			<Component {...pageProps} />
 		</Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -4,14 +4,47 @@ export const Container = styled("div", {
 	display: "flex",
 	flexDirection: "column",
 	alignItems: "flex-start",
-	justifyContent: "center",
 	minHeight: "100vh",
-
 })
 
 export const Header = styled("header", {
 	padding: "2rem 0",
 	width: "100%",
 	maxWidth: 1180,
-	margin: "0 auto"
+	margin: "0 auto",
+	display: "flex",
+	justifyContent: "space-between",
+	alignItems: "center"
+})
+
+export const ShoppingCartIcon = styled("div",{
+	width: "3rem",
+	height: "3rem",
+	backgroundColor: "$gray800",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	borderRadius: 6,
+	color: "$gray500",
+	cursor: "pointer",
+	position: "relative",
+
+	"&:hover":{
+		filter: "brightness(140%)",
+		transition: "0.2s",
+	},
+
+	span:{
+		position:"absolute",
+		right: -7,
+		top: -7,
+		width: 24,
+		height: 24,
+		textAlign:"center",
+		borderRadius: "50%",
+		border: "3px solid $gray900",
+		background: "$green500",
+		color: "$white"
+	}
+
 })
